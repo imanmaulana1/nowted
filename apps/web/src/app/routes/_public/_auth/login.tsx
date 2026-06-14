9ff5f436e2679e5c9a2d7d3711dc29fa943704c6
Iman Maulana <maulanaiman902@gmail.com>
2026-06-14 11:18:58 +0700

    feat(auth-web): add login page

--- apps/web/src/app/routes/_public/_auth/login.tsx
+++ apps/web/src/app/routes/_public/_auth/login.tsx
@@ -1,9 +1,24 @@
 import { createFileRoute } from '@tanstack/react-router'
 
+import { AuthCard } from '@/features/auth/components/auth-card'
+import { LoginForm } from '@/features/auth/components/login-form'
+
 export const Route = createFileRoute('/_public/_auth/login')({
   component: LoginPage,
 })
 
 function LoginPage() {
-  return <div>Hello "/_public/_auth/login"!</div>
+  return (
+    <AuthCard
+      title='Welcome back'
+      description='Sign in to your account'
+      direction='bottom'
+      footer={{
+        text: "Dont't have an account?",
+        linkHref: '/register',
+        linkText: 'register',
+      }}>
+      <LoginForm />
+    </AuthCard>
+  )
 }
---
+++ apps/web/src/features/auth/components/login-form.tsx
@@ -0,0 +1,118 @@
+import { useForm } from '@tanstack/react-form'
+import { toast } from 'sonner'
+
+import { Button } from '@/shared/components/ui/button'
+import {
+  Field,
+  FieldError,
+  FieldGroup,
+  FieldLabel,
+} from '@/shared/components/ui/field'
+import { Input } from '@/shared/components/ui/input'
+import { InputPassword } from '@/shared/components/ui/input-password'
+import { Spinner } from '@/shared/components/ui/spinner'
+
+import { loginSchema, type LoginInput } from '../schemas/login.schema'
+
+export function LoginForm() {
+  const form = useForm({
+    defaultValues: {
+      email: '',
+      password: '',
+    } as LoginInput,
+    validators: {
+      onSubmit: loginSchema,
+    },
+    onSubmit: async ({ value }) => {
+      await new Promise((resolve) => setTimeout(resolve, 800))
+      form.reset()
+      toast.success(`Welcome back! 👋`, {
+        description: 'Ready to pick up where you left off',
+      })
+      console.log(value)
+    },
+  })
+
+  const handleSubmit = (e: React.SubmitEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    form.handleSubmit()
+  }
+
+  return (
+    <form id='login-form' className='w-full' noValidate onSubmit={handleSubmit}>
+      <FieldGroup>
+        <form.Field
+          name='email'
+          children={(field) => {
+            const isInvalid =
+              field.state.meta.isTouched && !field.state.meta.isValid
+
+            const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+              field.handleChange(e.target.value)
+            }
+
+            return (
+              <Field data-invalid={isInvalid}>
+                <FieldLabel htmlFor={field.name}>Email</FieldLabel>
+                <Input
+                  id={field.name}
+                  name={field.name}
+                  value={field.state.value}
+                  onBlur={field.handleBlur}
+                  onChange={handleChange}
+                  aria-invalid={isInvalid}
+                  placeholder='john.doe@gmail.com'
+                  autoComplete='email'
+                />
+                {isInvalid && <FieldError errors={field.state.meta.errors} />}
+              </Field>
+            )
+          }}
+        />
+
+        <form.Field
+          name='password'
+          children={(field) => {
+            const isInvalid =
+              field.state.meta.isTouched && !field.state.meta.isValid
+
+            const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+              field.handleChange(e.target.value)
+            }
+
+            return (
+              <Field data-invalid={isInvalid}>
+                <FieldLabel htmlFor={field.name}>Password</FieldLabel>
+                <InputPassword
+                  id={field.name}
+                  name={field.name}
+                  value={field.state.value}
+                  onBlur={field.handleBlur}
+                  onChange={handleChange}
+                  aria-invalid={isInvalid}
+                  placeholder='********'
+                  autoComplete='current-password'
+                />
+                {isInvalid && <FieldError errors={field.state.meta.errors} />}
+              </Field>
+            )
+          }}
+        />
+      </FieldGroup>
+      <div className='mt-6'>
+        <form.Subscribe
+          selector={(state) => [state.canSubmit, state.isSubmitting]}
+          children={([canSubmit, isSubmitting]) => (
+            <Button
+              type='submit'
+              disabled={!canSubmit || isSubmitting}
+              className='w-full'>
+              {isSubmitting && <Spinner />}
+              {isSubmitting ? 'Signing in...' : 'Sign in'}
+            </Button>
+          )}
+        />
+      </div>
+    </form>
+  )
+}
